9c34b4223c353770fe08905d698de0912bcc1eb1
Chenming C <43266446+chen622@users.noreply.github.com>
2020-12-27 16:17:44 +0800

    feat: remvoe unnecessary files

--- test-other.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-javac -encoding UTF-8 -cp . ticketingsystem/TestOther.java
-java -cp . ticketingsystem/TestOther
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-javac -encoding UTF-8 -cp . ticketingsystem/Test.java
-java -cp . ticketingsystem/Test
